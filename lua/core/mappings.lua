15c3cf3159b70dc11e83b48765b00577843b1aa0
Bari Muginov <>
2024-12-01 00:33:36 +0300

    add mappings and disable fzf

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,43 +1,58 @@
+local keymap = vim.keymap
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("x", "<leader>p", '"_dP')
+keymap.set("x", "<leader>p", '"_dP')
+
+-- not working. Find out why. Tried :checkhealth and it says that everything is ok with clipboard (pbcopy is used);
+-- also checked :registers
+-- keymap.set("n", "<leader>y", '"+y')
+-- keymap.set("v", "<leader>y", '"+y')
+-- keymap.set("n", "<leader>Y", '"+Y')
+
 -- leader
 vim.g.mapleader = " "
 
 -- insert
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("i", "jl", "<Esc>")
+keymap.set("i", "jj", "<Esc>")
+keymap.set("i", "jk", "<Esc>")
+keymap.set("i", "jl", "<Esc>")
+
+-- jumps
+keymap.set("n", "<c-i>", "<c-s-i>") -- telescope remaps <c-i> and telescope ["<C-i>"] = false doesn't help
 
 -- motions
-vim.keymap.set("i", "<c-l>", "<Right>")
-vim.keymap.set("i", "<c-h>", "<Left>")
-vim.keymap.set("i", "<c-j>", "<Down>")
-vim.keymap.set("i", "<c-k>", "<Up>")
-vim.keymap.set("t", "<c-l>", "<Right>")
-vim.keymap.set("t", "<c-h>", "<Left>")
-vim.keymap.set("t", "<c-j>", "<Down>")
-vim.keymap.set("t", "<c-k>", "<Up>")
+keymap.set("i", "<c-l>", "<Right>")
+keymap.set("i", "<c-h>", "<Left>")
+keymap.set("i", "<c-j>", "<Down>")
+keymap.set("i", "<c-k>", "<Up>")
+keymap.set("t", "<c-l>", "<Right>")
+keymap.set("t", "<c-h>", "<Left>")
+keymap.set("t", "<c-j>", "<Down>")
+keymap.set("t", "<c-k>", "<Up>")
 
 -- buffers
-vim.keymap.set("n", "<leader>w", ":w<CR>")
+keymap.set("n", "<leader>w", ":w<CR>")
 
 -- diagnostic
-vim.keymap.set("n", "gh", vim.lsp.buf.hover, { desc = "Open information about the symbol" })
-vim.keymap.set("n", "gi", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "gI", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+keymap.set("n", "gh", vim.lsp.buf.hover, { desc = "Open information about the symbol" })
+keymap.set("n", "gi", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+keymap.set("n", "gI", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- neo-tree
-vim.keymap.set("n", "<leader>e", ":Neotree toggle float<CR>")
-vim.keymap.set("n", "<leader>E", ":Neotree reveal float<CR>")
+keymap.set("n", "<leader>e", ":Neotree toggle float<CR>")
+keymap.set("n", "<leader>E", ":Neotree reveal float<CR>")
 
 -- mini-pick
--- vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
--- vim.keymap.set('n', '<leader><leader>', ':Pick grep_live<CR>')
--- vim.keymap.set('n', '<leader>b', ':Pick buffers<CR>')
+-- keymap.set('n', '<leader>f', ':Pick files<CR>')
+-- keymap.set('n', '<leader><leader>', ':Pick grep_live<CR>')
+-- keymap.set('n', '<leader>b', ':Pick buffers<CR>')
 
 -- mini-trailspace
-vim.keymap.set("n", "<leader>ts", ":lua MiniTrailspace.trim()<CR>")
+keymap.set("n", "<leader>ts", ":lua MiniTrailspace.trim()<CR>")
 
 -- mini-visits
--- vim.keymap.set(
+-- keymap.set(
 -- 	"n",
 -- 	"<Tab>",
 -- 	-- ":lua MiniVisits.select_path(nil, {sort = MiniVisits.gen_sort.default({recency_weight = 1})})"
@@ -45,46 +60,46 @@ vim.keymap.set("n", "<leader>ts", ":lua MiniTrailspace.trim()<CR>")
 -- )
 
 -- navigation
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+keymap.set("n", "<c-k>", ":wincmd k<CR>")
+keymap.set("n", "<c-j>", ":wincmd j<CR>")
+keymap.set("n", "<c-h>", ":wincmd h<CR>")
+keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- splits
-vim.keymap.set("n", "|", ":vsplit<CR>")
-vim.keymap.set("n", "\\", ":split<CR>")
+keymap.set("n", "|", ":vsplit<CR>")
+keymap.set("n", "\\", ":split<CR>")
 
 -- tabs
---vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
---vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
---vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
-vim.keymap.set("n", "L", ":bnext<CR>")
-vim.keymap.set("n", "H", ":bprev<CR>")
-vim.keymap.set("n", "<leader>d", ":bdelete<CR>")
+--keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+--keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+--keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
+keymap.set("n", "L", ":bnext<CR>")
+keymap.set("n", "H", ":bprev<CR>")
+keymap.set("n", "<leader>d", ":bdelete<CR>")
 
 -- git
-vim.keymap.set("n", "<leader>g", ":LazyGit<CR>")
+keymap.set("n", "<leader>g", ":LazyGit<CR>")
 
 -- fzf
--- vim.keymap.set("n", "<leader>f", ":FzfLua files<CR>")
--- vim.keymap.set("n", "<leader>/", ":FzfLua live_grep<CR>")
-vim.keymap.set("n", "<Tab>", ":FzfLua buffers<CR>")
-vim.keymap.set("n", "<leader><leader>", ":FzfLua buffers<CR>")
--- vim.keymap.set("n", "gr", ":FzfLua lsp_references<CR>")
--- vim.keymap.set("n", "<leader>fh", ":FzfLua helptags<CR>")
--- vim.keymap.set("n", "<leader>/", ":FzfLua live_grep resume=true<CR>")
--- vim.keymap.set("n", "<leader>r", ":FzfLua resume<CR>")
+-- keymap.set("n", "<leader>f", ":FzfLua files<CR>")
+-- keymap.set("n", "<leader>/", ":FzfLua live_grep<CR>")
+-- keymap.set("n", "<Tab>", ":FzfLua buffers<CR>")
+-- keymap.set("n", "<leader><leader>", ":FzfLua buffers<CR>")
+-- keymap.set("n", "gr", ":FzfLua lsp_references<CR>")
+-- keymap.set("n", "<leader>fh", ":FzfLua helptags<CR>")
+-- keymap.set("n", "<leader>/", ":FzfLua live_grep resume=true<CR>")
+-- keymap.set("n", "<leader>r", ":FzfLua resume<CR>")
 
 -- toggleterm
 function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], opts)
-	-- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-	-- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-	-- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-	-- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-	-- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-	-- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+	keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], opts)
+	-- keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	-- keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	-- keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	-- keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	-- keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+	-- keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
@@ -97,4 +112,4 @@ vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 vim.api.nvim_set_keymap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
 
 -- other
-vim.keymap.set("n", "<leader>q", ":q<CR>")
+keymap.set("n", "<leader>q", ":q<CR>")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,40 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
+	-- tag = "0.1.8",
+	branch = "master",
 	-- or                              , branch = '0.1.x',
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("telescope").setup({
+			-- any picker (buffers, lsp_references, pickers, git_status etc.) except
+			-- its own options also supports default options (initial_mode, cache_picker etc.)
 			pickers = {
 				pickers = {
 					initial_mode = "normal",
+				},
+				find_files = {
+					cache_picker = {
+						ignore_empty_prompt = true,
+					},
+				},
+				live_grep = {
+					cache_picker = {
+						ignore_empty_prompt = true,
+					},
 				},
 				buffers = {
 					ignore_current_buffer = true,
 					sort_mru = true,
 					initial_mode = "normal",
+					cache_picker = {
+						ignore_empty_prompt = true,
+					},
+				},
+				lsp_definitions = {
+					initial_mode = "normal",
+					cache_picker = {
+						ignore_empty_prompt = true,
+					},
 				},
 				lsp_references = {
 					initial_mode = "normal",
@@ -28,8 +50,7 @@ return {
 						["L"] = require("telescope.actions").cycle_history_next,
 						["H"] = require("telescope.actions").cycle_history_prev,
 						["q"] = require("telescope.actions").close,
-						["l"] = require("telescope.actions").select_default,
-						-- or ["l"] = "select_default"
+						["l"] = require("telescope.actions").select_default, -- or ["l"] = "select_default"
 					},
 				},
 				cache_picker = {
@@ -40,7 +61,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
-		-- vim.keymap.set("n", "<Tab>", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<Tab>", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Telescope lsp references" })
 		vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Telescope lsp definitions" })
