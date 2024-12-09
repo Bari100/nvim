9ec04a16499d5b60c12394e8e47842fa58f687a5
Bari Muginov <>
2024-12-09 17:59:09 +0300

    fix cursor, add new markdown renderer

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -6,9 +6,13 @@ vim.wo.relativenumber = true
 vim.opt["tabstop"] = 2
 vim.opt["shiftwidth"] = 2
 
+-- cursor
+vim.opt.guicursor =
+	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
 -- other
 vim.opt.scrolloff = 8
 vim.opt.wrap = false
 vim.opt.termguicolors = true
-vim.opt.spelllang = "en_us"
+vim.opt.spelllang = { "en_us", "ru_ru" }
 vim.opt.spell = true
