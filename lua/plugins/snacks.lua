74c3fc546c79335eca45c7af185348c1a5a993df
Bari Muginov <>
2025-02-21 03:36:36 +0300

    add some helix keymaps, add flash and snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,42 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		-- bigfile = { enabled = true },
+		-- dashboard = { enabled = true },
+		-- explorer = { enabled = true },
+		-- indent = { enabled = true },
+		-- input = { enabled = true },
+		-- picker = { enabled = true },
+		-- notifier = { enabled = true },
+		-- quickfile = { enabled = true },
+		-- scope = { enabled = true },
+		-- scroll = { enabled = true },
+		-- statuscolumn = { enabled = true },
+		-- words = { enabled = true },
+		scratch = {
+			ft = "markdown",
+		},
+	},
+	keys = {
+		{
+			"<leader>..",
+			function()
+				Snacks.scratch()
+			end,
+			desc = "Toggle Scratch Buffer",
+		},
+		{
+			"<leader>.s",
+			function()
+				Snacks.scratch.select()
+			end,
+			desc = "Select Scratch Buffer",
+		},
+	},
+}
