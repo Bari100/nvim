53825700c0050c93ed7563a504279e18592afc38
Bari Muginov <>
2025-02-26 12:47:12 +0300

    fix deleting buffer

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,14 +1,14 @@
 -- bufdelete ----------------------------------------------------
 vim.api.nvim_create_user_command("Bc", function()
-	Snacks.bufdelete.delete()
+	Snacks.bufdelete.delete({ wipe = true })
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("Bca", function()
-	Snacks.bufdelete.all()
+	Snacks.bufdelete.all({ wipe = true })
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("Bco", function()
-	Snacks.bufdelete.other()
+	Snacks.bufdelete.other({ wipe = true })
 end, { nargs = 0 })
 
 -- lazygit ----------------------------------------------------
